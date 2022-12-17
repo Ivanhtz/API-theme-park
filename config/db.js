2fbfe8bebbbbcde001d603d40e86e5b9c6ec2637
Ivan Pizarroso <ivnp31@gmail.com>
2022-12-17 11:13:31 +0100

    set en true

--- config/db.js
+++ config/db.js
@@ -1,3 +1,6 @@
 const mongoose = require("mongoose");
+mongoose.set("strictQuery", true);
 
-mongoose.connect(process.env.MONGO_URL);
+mongoose.connect(process.env.MONGO_URL, () => {
+  console.log("Conectado con MongoDB");
+});
